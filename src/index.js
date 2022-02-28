05d4503bcee5774dc53e6da8794898bc05b9e338
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-02-28 15:28:57 -0600

    Adapt home page to wireframe and add section recent scores

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
 import './style.css';
-
-function component() {
-  const element = document.createElement('div');
-
-  // Lodash, now imported by this script
-  element.innerHTML = 'Hello webpack';
-
-  return element;
-}
-
-document.body.appendChild(component());
