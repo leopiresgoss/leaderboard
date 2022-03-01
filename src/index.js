8f6cd9df70d3540c2067b563accbf536c4fc1aa1
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-03-01 11:33:20 -0600

    Add startNewGame function
    With this function, I can create a new game using Leaderboard API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './style.css';
+
+// create a new game
+const gameName = 'Leader Gamer';
+
+const startNewGame = async (gameName) => {
+  const requestURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+  const res = await fetch(requestURL, {
+    method: 'POST',
+    body: JSON.stringify({
+      name: `${gameName}`,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  const newGame = await res.json();
+
+  return newGame;
+};
+
+startNewGame(gameName);
