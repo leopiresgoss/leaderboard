837e0ebf23a282b6dc57b64a5ea5b569dc484765
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-03-01 14:00:48 -0600

    Add function to add score to game

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './style.css';
 
 // create a new game
 const gameName = 'Leader Gamer';
+const requestURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
 
 const startNewGame = async (gameName) => {
   const requestURL = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
@@ -31,4 +32,38 @@ const getId = async () => {
   return undefined;
 };
 
-getId();
+// add to score to game
+const addScoreToGame = async () => {
+  const id = await getId();
+
+  const url = `${requestURL}${id}/scores`;
+
+  // first test
+  let res = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: 'Juan',
+      score: 500,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  // second test
+  res = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: 'Lisa',
+      score: 100,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  const result = await res.json();
+  return result;
+};
+
+addScoreToGame();
